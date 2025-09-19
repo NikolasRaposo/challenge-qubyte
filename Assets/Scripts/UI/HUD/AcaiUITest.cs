690dd75ba0c245089cb0fa0b12d59fed8e472ec1
Nikolas Raposo Martins Vieira <nikolasraposo@gmail.com>
2025-09-18 21:37:39 -0300

    Error UI Adjustment

--- Assets/Scripts/UI/HUD/AcaiUITest.cs
+++ Assets/Scripts/UI/HUD/AcaiUITest.cs
@@ -1,24 +1,25 @@
 using UnityEngine;
-
-public class AcaiUITest : MonoBehaviour
-{
-    [SerializeField] private CoinUIAnimation coinUI; // arrasta aqui o objeto com CoinUIAnimation
-    [SerializeField] private int coinCount = 0;
-
-    void Update()
+namespace UI.HUD {
+    public class AcaiUITest : MonoBehaviour
     {
-        // Quando apertar espaço, simula pegar uma moeda
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            coinCount++;
-            coinUI.OnCollect(coinCount);
-        }
+        [SerializeField] private CoinUIAnimation coinUI; // arrasta aqui o objeto com CoinUIAnimation
+        [SerializeField] private int coinCount;
 
-        // Quando apertar C, reseta moedas
-        if (Input.GetKeyDown(KeyCode.C))
+        void Update()
         {
-            coinCount = 0;
-            coinUI.OnCollect(coinCount);
+            // Quando apertar espaço, simula pegar uma moeda
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                coinCount++;
+                //coinUI.OnCollect(coinCount);
+            }
+
+            // Quando apertar C, reseta moedas
+            if (Input.GetKeyDown(KeyCode.C))
+            {
+                coinCount = 0;
+                //coinUI.OnCollect(coinCount);
+            }
         }
     }
 }
